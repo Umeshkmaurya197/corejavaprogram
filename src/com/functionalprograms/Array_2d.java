bd612eb26067871fb42b433bb1f663a3a3f8df90
Umeshkmaurya197 <umeshkumarmaurya.98@gmail.com>
2022-07-01 23:20:50 +0530

    refactored : PrintWriter Added here

--- src/com/functionalprograms/Array_2d.java
+++ src/com/functionalprograms/Array_2d.java
@@ -1,41 +1,40 @@
 package com.functionalprograms;
 
+import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Array_2d {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println(" -------- Welcome to 2d - Array --------\n");
-				
+
 		Scanner scanner = new Scanner(System.in);
-	//	PrintWriter out = new PrintWriter(System.out);
-		
+		PrintWriter writer = new PrintWriter(System.out);
+
 		System.out.print(" Enter the number of rows :");
 		int rows = scanner.nextInt();
 		System.out.print(" Enter the number of  cols :");
 		int cols = scanner.nextInt();
-		
-		int [][] arr = new int[rows][cols];
-		
-		for(int i = 0 ; i < rows ; i++)
-		{
+
+		int[][] arr = new int[rows][cols];
+
+		for (int i = 0; i < rows; i++) {
 			System.out.println(" \n");
-			for(int j = 0 ; j < cols; j++)
-			{
-				System.out.print(" Enter the array data at location ["+i+"]["+j+"]  : " );
-				arr[i][j]=scanner.nextInt();
+			for (int j = 0; j < cols; j++) {
+				System.out.print(" Enter the array data at location [" + i + "][" + j + "]  : ");
+				arr[i][j] = scanner.nextInt();
 			}
 		}
-		
-		for ( int i = 0 ; i < rows ; i++ )
-     	{
-			System.out.println("\n");
-			for( int j = 0 ; j < cols ; j++ )
-			{   
-				System.out.print(" "+arr[i][j]);    //"arr["+i+"]["+j+"]  = "
- 			}
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				writer.write(" " + arr[i][j]);
+			}
+			writer.write("\n");
+
 		}
+		writer.flush();
+		writer.close();
 		scanner.close();
 	}
 }
